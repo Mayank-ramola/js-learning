f0100b517e4e5c8a09031285b08744f43c7ed6c0
Mayank-ramola <mayankramola1909@gmail.com>
2026-08-24 04:59:35 +0000

    conversion of datatypes

---
+++ 1_basics/datatype_conversion.js
@@ -0,0 +1,26 @@
+let score = "1323"  // if value is null then output will be 0 and type is object
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "133" = 133
+// "133acd" = NaN (not a number)
+// true = 1 / false = 0
+
+let isLoggedIn = 1
+
+console.log(typeof isLoggedIn)
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 = true 
+// 0 = false
+// "" = false
+// "Name" = true
