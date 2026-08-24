4010f19bf924021769fd34bf53b5e275b92a95c4
Mayank-ramola <mayankramola1909@gmail.com>
2026-08-24 04:40:54 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3 + 3) // we are using nodejs not browser 
+
+let name = "Mayank"
+let age = 20
+let userLoggedIn = false
+
+/*
+number = 2 to power 53
+bigint = bogger digits or numbers
+string = ""
+boolean = true/false
+null = standalone value
+undefined
+symbol
+*/
+
+console.log(typeof age)
+console.log(typeof null) // null is object
+console.log(typeof "Mayank")
