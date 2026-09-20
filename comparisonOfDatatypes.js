3575a5194490a3e166200d73b975ace1c0ad6b1e
Mayank-ramola <mayankramola1909@gmail.com>
2026-09-20 16:31:00 +0530

    Add files via upload

---
+++ comparisonOfDatatypes.js
@@ -0,0 +1,30 @@
+// console.log(2<4);
+// console.log(2>4);
+// console.log(2<=4); 
+// console.log(2>=4); 
+// console.log(2==4);
+// console.log(2!=4);
+
+
+console.log(2==="2");
+console.log(2=="2");
+
+console.log(null == 0); // False
+console.log(null < 0); // False
+console.log(null > 0); // False
+console.log(null <= 0); // True
+console.log(null >= 0); // True
+
+
+console.log(undefined == 0); // False
+console.log(undefined < 0);   // False
+console.log(undefined > 0); // False
+console.log(undefined <= 0); // False
+console.log(undefined >= 0); // False
+
+
+// === - checks for both value and datatype
+
+console.log(2 === "2"); // False
+console.log(2 === 2); // True  
+ 
