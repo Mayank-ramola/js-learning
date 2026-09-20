e48868c19fa7b462f5e2dec64dda9a744d7c526c
Mayank-ramola <mayankramola1909@gmail.com>
2026-09-20 16:49:38 +0530

    Add files via upload
    summary of datatypes

---
+++ datatypessummary.js
@@ -0,0 +1,30 @@
+"use striict";
+// #Primitive Datatypes
+// 7 Types of Primitive Datatypes in JavaScript : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const name ="Mayank"
+const number = 123.3
+
+const IsLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 448434871254878421n
+
+
+
+// #Reference (non-primitive) Datatypes
+// 3 Types of Reference Datatypes in JavaScript : Object, Array, Function   
+
+const Heros = ["ironman", "spiderman", "hulk"];
+let myObj = {
+    name : "Mayank",
+    age : 20,
+}
+
+const myFuunction = function(){
+    console.log("hello world");
+}
